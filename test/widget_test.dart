b73b74df2555f53f942b453f9148d9ac7884acce
SoLo <48683374+iKunalmathur@users.noreply.github.com>
2019-12-08 11:23:15 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cloudy24_webview/main.dart';
+import 'package:flutterapp_webview/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
